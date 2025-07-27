aed1b6f8fad4a915e6e97eabd7fc8a387fe3447d
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-07-27 23:42:15 +0100

    done

--- main.lua
+++ main.lua
@@ -131,37 +131,163 @@ local pieceStructures = {
 
 function love.load()
 	love.graphics.setBackgroundColor(255, 255, 255)
-
 	gridXCount = 10
 	gridYCount = 18
 
-	inert = {}
-	for y = 1, gridYCount do
-		inert[y] = {}
-		for x = 1, gridXCount do
-			inert[y][x] = " "
+	pieceType = 1
+	pieceRotation = 1
+	pieceX = 3
+	pieceY = 0
+	pieceXCount = 4
+	pieceYCount = 4
+
+	timerLimit = 0.5
+
+	function canPieceMove(testX, testY, testRotation)
+		for y = 1, pieceYCount do
+			for x = 1, pieceXCount do
+				local testBlockX = testX + x
+				local testBlockY = testY + y
+				if
+					pieceStructures[pieceType][testRotation][y][x] ~= " "
+					and (
+						testBlockX < 1
+						or testBlockX > gridXCount
+						or testBlockY > gridYCount
+						or inert[testBlockY][testBlockX] ~= " "
+					)
+				then
+					return false
+				end
+			end
+		end
+		return true
+	end
+
+	function newSequence()
+		sequence = {}
+		for pieceTypeIndex = 1, #pieceStructures do
+			local position = love.math.random(#sequence + 1)
+			table.insert(sequence, position, pieceTypeIndex)
 		end
 	end
 
-	pieceType = 1
-	pieceRotation = 1
-  pieceX = 3
-  pieceY = 0
+	function newPiece()
+		pieceX = 3
+		pieceY = 0
+		pieceRotation = 1
+		pieceType = table.remove(sequence)
+
+		if #sequence == 0 then
+			newSequence()
+		end
+	end
+
+	function reset()
+		inert = {}
+		for y = 1, gridYCount do
+			inert[y] = {}
+			for x = 1, gridXCount do
+				inert[y][x] = " "
+			end
+		end
+
+		newSequence()
+		newPiece()
+
+		timer = 0
+	end
+
+	reset()
 end
 
-function love.update() end
+function love.update(dt)
+	timer = timer + dt
+	if timer >= timerLimit then
+		timer = 0
+
+		local testY = pieceY + 1
+		if canPieceMove(pieceX, testY, pieceRotation) then
+			pieceY = testY
+		else
+			for y = 1, pieceYCount do
+				for x = 1, pieceXCount do
+					local block = pieceStructures[pieceType][pieceRotation][y][x]
+					if block ~= " " then
+						inert[pieceY + y][pieceX + x] = block
+					end
+				end
+			end
+			for y = 1, gridYCount do
+				local complete = true
+				for x = 1, gridXCount do
+					if inert[y][x] == " " then
+						complete = false
+						break
+					end
+				end
+
+				if complete then
+					for removeY = y, 2, -1 do
+						for removeX = 1, gridXCount do
+							inert[removeY][removeX] = inert[removeY - 1][removeX]
+						end
+					end
+
+					for removeX = 1, gridXCount do
+						inert[1][removeX] = " "
+					end
+				end
+			end
+
+			newPiece()
+
+			if not canPieceMove(pieceX, pieceY, pieceRotation) then
+        reset()
+			end
+		end
+	end
+end
 
 function love.keypressed(key)
 	if key == "x" then
-		pieceRotation = pieceRotation + 1
-		if pieceRotation > #pieceStructures[pieceType] then
-			pieceRotation = 1
+		local testRotation = pieceRotation + 1
+		if testRotation > #pieceStructures[pieceType] then
+			testRotation = 1
+		end
+
+		if canPieceMove(pieceX, pieceY, testRotation) then
+			pieceRotation = testRotation
 		end
 	elseif key == "z" then
-		pieceRotation = pieceRotation - 1
-		if pieceRotation < 1 then
-			pieceRotation = #pieceStructures[pieceType]
+		local testRotation = pieceRotation - 1
+		if testRotation < 1 then
+			testRotation = #pieceStructures[pieceType]
 		end
+
+		if canPieceMove(pieceX, pieceY, testRotation) then
+			pieceRotation = testRotation
+		end
+	elseif key == "left" then
+		local testX = pieceX - 1
+
+		if canPieceMove(pieceX, pieceY, pieceRotation) then
+			pieceX = testX
+		end
+	elseif key == "right" then
+		local testX = pieceX + 1
+
+		if canPieceMove(pieceX, pieceY, pieceRotation) then
+			pieceX = testX
+		end
+	elseif key == "c" then
+		while canPieceMove(pieceX, pieceY + 1, pieceRotation) do
+			pieceY = pieceY + 1
+			timer = timerLimit
+		end
+	elseif key == "s" then
+		newSequence()
+		print(table.concat(sequence, ", "))
 	end
 end
 
@@ -176,6 +302,7 @@ function love.draw()
 			s = { 0.83, 0.54, 0.93 },
 			t = { 0.97, 0.58, 0.77 },
 			z = { 0.66, 0.83, 0.46 },
+			preview = { 0.75, 0.75, 0.75 },
 		}
 		local color = colors[block]
 		love.graphics.setColor(color)
@@ -185,17 +312,29 @@ function love.draw()
 		love.graphics.rectangle("fill", (x - 1) * blockSize, (y - 1) * blockSize, blockDrawSize, blockDrawSize)
 	end
 
-	for y = 1, gridYCount do
-		for x = 1, gridXCount do
-			drawBlock(inert[y][x], x, y)
+	local offsetX = 2
+	local offsetY = 5
+
+	for y = 1, pieceYCount do
+		for x = 1, pieceXCount do
+			local block = pieceStructures[sequence[#sequence]][1][y][x]
+			if block ~= " " then
+				drawBlock("preview", x + 5, y + 1)
+			end
 		end
 	end
 
-	for y = 1, 4 do
-		for x = 1, 4 do
+	for y = 1, gridYCount do
+		for x = 1, gridXCount do
+			drawBlock(inert[y][x], x + offsetX, y + offsetY)
+		end
+	end
+
+	for y = 1, pieceYCount do
+		for x = 1, pieceXCount do
 			local block = pieceStructures[pieceType][pieceRotation][y][x]
 			if block ~= " " then
-				drawBlock(block, x, y)
+				drawBlock(block, x + pieceX + offsetX, y + pieceY + offsetY)
 			end
 		end
 	end
